4417b73c34d9b134644e62e1b5a32d2f4d56ebe3
Tony Thompson <penguinprofession@icloud.com>
2018-05-03 14:15:41 -0400

    refactored for readability

--- server.js
+++ server.js
@@ -24,27 +24,24 @@ app.get('/messages', (req, res) => {
     })
 })
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
     const message = new Message(req.body)
 
-    message.save()
-    .then(() =>{
-        console.log('saved')
-       return  Message.findOne({ message: 'badword' })
-    })
-    .then( censored => {
-        if (censored) {
-            console.log('censored words found', censored)
-            return message.remove({ _id: censored.id })
-        }
-        io.emit('message', req.body)
-        res.sendStatus(200)
+    const savedMessage = await message.save()
 
-    })
-    .catch((err) => {
-        res.sendStatus(500)
-        return console.error(err)
-    })
+    console.log('saved')
+
+    const censored = await Message.findOne({ message: 'badword' })
+        if (censored) 
+            await message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+            res.sendStatus(200)
+
+    //         .catch((err) => {
+    //             res.sendStatus(500)
+    //             return console.error(err)
+    // })
 })
 
 io.on('connection', (socket) => {
