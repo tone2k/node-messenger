dd93137d0c8690c1580959659c0077d4879275d7
Tony Thompson <penguinprofession@icloud.com>
2018-05-03 11:44:35 -0400

    created get route with document ready function

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ $(() => {
     $('#send').click(()=>{
         addMessages({name: 'Tim', message: 'Hello'}) 
     });
+    getMessages();
 });
 
 function addMessages(message){
     $('#messages').append(`<h4> ${message.name} </h4> <p> ${message.message} </p>`)
+}
+
+function getMessages() {
+    $.get('http://localhost:3000/messages', (data) => {
+        console.log(data)
+    })
 }
--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname))
+
+const messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Tom', message: 'Hey'},
+    {name: 'Tam', message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+
 const server = app.listen(3000, () => {
     console.log('server is listening on port: ', server.address().port)
 })
