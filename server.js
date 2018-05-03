5fe18170b2d1d8595c479f63497db6593823a82e
Tony Thompson <penguinprofession@icloud.com>
2018-05-03 10:43:28 -0400

    added in callback for console log server is listening

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname))
-app.listen(3000)
+const server = app.listen(3000, () => {
+    console.log('server is listening on port: ', server.address().port)
+})
