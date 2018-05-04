8589785e0c382451090113e9699a615d85007a20
Tony Thompson <penguinprofession@icloud.com>
2018-05-04 09:16:51 -0400

    experimented with TDD practices on user 'tim'

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -17,7 +17,25 @@ describe('get messages', () =>{
             request.get('http://localhost:3000/messages', (err, res) => {
                 expect(JSON.parse(res.body).legnth).toBeGreaterThan(0)
                 done()
+            })
         })
     })
 })
+
+describe('get messages from a user', () =>{
+    it('should return 200 OK', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            console.log(res.body)
+            expect(res.statusCode).toEqual(200)
+            done()
+        })
+    })
+    it('name should be john', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            console.log(res.body)
+            expect(JSON.parse(res.body)[0].name).toEqual('tim')
+            done()
+        })
+        
+    })
 })
